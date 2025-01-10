30152e0e8134d974ec69ec7df5b2ba5ea5c58886
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-01-09 23:54:16 -0300

    Add files to repository

---
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewViewModel.swift
+//  ToDoList
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 09/01/25.
+//
+
+import Foundation
---
+++ ToDoList/Views/ToDoListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListItemView.swift
+//  ToDoList
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 09/01/25.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
