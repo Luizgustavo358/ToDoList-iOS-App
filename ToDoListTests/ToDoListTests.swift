2af91ff4ed96dd220f2335f255721504e6a6c39c
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-01-09 22:23:37 -0300

    Initial Commit

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 09/01/25.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
