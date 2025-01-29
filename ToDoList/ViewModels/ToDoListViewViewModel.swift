d00f25f38c883604ea0185e713fb327605cb8a2f
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-01-28 21:39:07 -0300

    deleting the items

--- ToDoList/ViewModels/ToDoListViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by Luiz Gustavo Bragança dos Santos on 09/01/25.
 //
 
+import FirebaseFirestore
 import Foundation
 
 /**
@@ -14,9 +15,23 @@ import Foundation
 class ToDoListViewViewModel: ObservableObject {
     @Published var showingNewItemView = false
     
-    init() {}
+    private let userId: String
     
+    init(userId: String) {
+        self.userId = userId
+    }
+    
+    /**
+     *  Delete to do list item
+     *  - Parameter id: item id to delete
+     */
     func delete(id: String) {
+        let db = Firestore.firestore()
         
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
     }
 }
--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -9,12 +9,18 @@ import FirebaseFirestore
 import SwiftUI
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
+    @StateObject var viewModel: ToDoListViewViewModel
     @FirestoreQuery var items: [ToDoListItem]
     
     init(userId: String) {
         // users/<id>/todos/<entries>
-        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos"
+        )
+        
+        self._viewModel = StateObject(
+            wrappedValue: ToDoListViewViewModel(userId: userId)
+        )
     }
     
     var body: some View {
@@ -26,7 +32,7 @@ struct ToDoListView: View {
                             Button("Delete") {
                                 viewModel.delete(id: item.id)
                             }
-                            .background(Color.red)
+                            .tint(.red)
                         }
                 }
             }
